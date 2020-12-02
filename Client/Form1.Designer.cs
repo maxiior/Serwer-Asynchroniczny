096c792f0308443719619e4fbeb72d6974bcfdf4
kbuczko <59309792+kbuczko@users.noreply.github.com>
2020-12-02 23:34:17 +0100

    Add files via upload

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 279);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 240);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Register";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 186);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(143, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Show users";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        NetworkStream stream;
+     
+        public Form1()
+        {
+            InitializeComponent();
+           
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient("127.0.0.1", 2048);
+            stream = client.GetStream();
+            textBox1.Text = "Connected to the server..";
+            getMessage(stream);
+        }
+        void SendMessage(NetworkStream stream, string message)
+        {
+           
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(message);
+            stream.Write(bytesToSend, 0, bytesToSend.Length);
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SendMessage(stream, textBox2.Text);
+            getMessage(stream);
+
+        }
+
+        void getMessage(NetworkStream stream)
+        {
+
+                String response = "";
+                Byte[] dataResponse = new Byte[256];
+                Int32 bytes = stream.Read(dataResponse, 0, dataResponse.Length);
+                response = System.Text.Encoding.ASCII.GetString(dataResponse, 0, bytes);
+                textBox1.Text = response;
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SendMessage(stream, textBox2.Text);
+            getMessage(stream);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SendMessage(stream, "3");
+            getMessage(stream);
+        }
+    }
+
+        
+}
