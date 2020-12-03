f96074672369e1376a723bfb50ac67b9238c4fc6
maxiior <59049370+maxiior@users.noreply.github.com>
2020-12-03 13:11:05 +0100

    testy

---
+++ ServerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("503ca7f3-4ae4-42bd-9a1f-2c139f3b8d82")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ServerTests/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerLibrary;
+using ServerAsyn;
+using System.Net;
+
+namespace ServerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestowanieBlednegoNumeruPortu()
+        {
+            try
+            {
+                Server<GACServerProtocol> server = new ServerTAP<GACServerProtocol>(IPAddress.Parse("127.0.0.1"), 48);
+                Assert.Fail();
+            }
+            catch (AssertFailedException)
+            {
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void TestowanieBlednegoNumeruIP()
+        {
+            try
+            {
+                Server<GACServerProtocol> server = new ServerTAP<GACServerProtocol>(IPAddress.Parse("127.0.0"), 2048);
+                Assert.Fail();
+            }
+            catch (AssertFailedException)
+            {
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void TestowanieMetodyCheckIfInDb()
+        {
+            try
+            {
+                Sqlite sql = new Sqlite();
+                sql.AddWin("kr");
+                Assert.Fail();
+            }
+            catch (AssertFailedException)
+            {
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
